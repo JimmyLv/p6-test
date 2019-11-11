f82995e73d6194e1baf606cf795a828e2c453e17
JimmyLv <jimmy.jinglv@gmail.com>
2019-11-11 20:25:57 +0800

    init project

---
+++ precache-manifest.6e1c42cbc88cd4351e1c591daf501e73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0b902fa9f04a3ddbd748d850bdd9f3",
+    "url": "/p6-test/index.html"
+  },
+  {
+    "revision": "316c2f0e6a0e2036ec99",
+    "url": "/p6-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b6614d9f35c26aafbca3",
+    "url": "/p6-test/static/js/2.f3c3a672.chunk.js"
+  },
+  {
+    "revision": "382c6910bd7d8dee7132",
+    "url": "/p6-test/static/js/3.cf30fe08.chunk.js"
+  },
+  {
+    "revision": "80b77e1f2bbf85ece3d2",
+    "url": "/p6-test/static/js/4.8da0bcbe.chunk.js"
+  },
+  {
+    "revision": "316c2f0e6a0e2036ec99",
+    "url": "/p6-test/static/js/main.3aad8f8b.chunk.js"
+  },
+  {
+    "revision": "6764161d077fcd203f31",
+    "url": "/p6-test/static/js/runtime~main.01e17c74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/p6-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
